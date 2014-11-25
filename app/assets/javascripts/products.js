e56c3098422e5e68a365e369211ad79d96cf7069
Cody House <codyahouse@gmail.com>
2014-11-25 11:31:27 -0500

    Completed infinite scroll

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,6 +1,6 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
 
 
 
@@ -18,13 +18,13 @@ $(document).on('page:load ready', function() {
 
 //detects a scroll place close to the bottom grabs teh url
 //makes an ajax request for the url we grabbed and places the next page on and on
-   if($('.pagination').length) {
-   $(window).scroll(function() {
+  if ($('.pagination').length) {
+    $(window).scroll(function() {
       var url = $('.pagination span.next').children().attr('href');
-      if (url && $(window).scrollTop() > ($(document).height() - $(window).height() - 50)) {
-      $('.pagination').text("Fetching more results...");
-       return $.getScript(url);
-     }
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+        $('.pagination').text("Fetching more products...");
+        return $.getScript(url);
+      }
     });
    }
- });
+  });
